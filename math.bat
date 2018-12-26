efa4c60c699e04efd7a194dbadf4396164cdee65
tosil-repo <repo@tosil-systems.com>
2018-12-26 09:53:59 +0530

    Gcovr command added

--- math.bat
+++ math.bat
@@ -1,5 +1,6 @@
 @echo off 
-g++ -fprofile-arcs -ftest-coverage -fPIC -O0 main.cpp math.cpp math.hpp -o math
+g++ -fprofile-arcs -ftest-coverage -fPIC -O0 main.cpp math.cpp -o math
 math.exe
-gcovr -r .
+gcovr -r . --html --html-details -o example-html-details.html
+
 
