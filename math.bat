ef28904a506ca3137700d8f517b513b983d5a773
tosil-repo <repo@tosil-systems.com>
2018-12-26 11:21:13 +0530

    Updated batch file

--- math.bat
+++ math.bat
@@ -2,5 +2,6 @@
 g++ -fprofile-arcs -ftest-coverage -fPIC -O0 main.cpp math.cpp -o math
 math.exe
 gcovr -r . --html --html-details -o example-html-details.html
+gcovr -r . --xml-pretty -o gcovr-report.xml
 
 
