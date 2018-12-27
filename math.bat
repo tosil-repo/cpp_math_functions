df5c7beedcb90d66c99e1c1153c5af97b751c974
tosil-repo <repo@tosil-systems.com>
2018-12-27 07:23:22 +0530

    1. Upadted math.bat file

--- math.bat
+++ math.bat
@@ -1,7 +1,4 @@
 @echo off 
-g++ -fprofile-arcs -ftest-coverage -fPIC -O0 main.cpp math.cpp -o math
-math.exe
-gcovr -r . --html --html-details -o example-html-details.html
-gcovr -r . --xml-pretty -o gcovr-report.xml
+g++  main.cpp math.cpp math.hpp -o math
 
 
