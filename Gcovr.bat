6b4ddc99769b3e813b25185118e9a0eaced2d0ef
tosil-repo <46019568+tosil-repo@users.noreply.github.com>
2018-12-27 15:16:10 +0530

    Update Gcovr.bat

--- Gcovr.bat
+++ Gcovr.bat
@@ -1,5 +1,5 @@
 @echo off
 g++ -fprofile-arcs -ftest-coverage -fPIC -O0 main.cpp math.cpp -o output.exe
 output.exe
-gcovr -r . --html --html-details -o example-html-details.html
-gcovr -r . --xml-pretty -o gcovr-report.xml
+gcovr -r . --html --html-details -o gcovr_report.html
+gcovr -r . --xml-pretty -o gcovr_report.xml
