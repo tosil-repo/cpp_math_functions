59ba254ce3e752e3eba630e5e62a4e7df8793107
tosil-repo <46019568+tosil-repo@users.noreply.github.com>
2018-12-27 16:36:44 +0530

    Delete Gcovr.bat

--- Gcovr.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-g++ -fprofile-arcs -ftest-coverage -fPIC -O0 main.cpp math.cpp -o output.exe
-output.exe
-gcovr -r . --html --html-details -o gcovr_report.html
-gcovr -r . --xml-pretty -o gcovr_report.xml
